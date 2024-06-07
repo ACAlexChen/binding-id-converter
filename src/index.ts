a1e023efed46f909d5ca6bcbbc30357203ecbde9
Alex Chen <m.ac.alexchen@outlook.com>
2024-06-08 01:17:29 +0800

    123

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from 'koishi'
+
+
 
 export const name = 'binding-id-converter'
 
-export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+class idconverter extends Service {
+  static inject = ['database']
+  constructor(ctx: Context) {
+    super(ctx, 'idconverter', true)
+  }
+
+  async pidConverter(pid: string, original_platform: string, target_platform) {
+    let aid = (await this.ctx.database.get('binding', {'pid': pid,'platform': original_platform},['aid']))[0].aid
+    let target_pid = (await this.ctx.database.get('binding',{'aid': aid,'platform': target_platform},['pid']))[0].pid
+    return target_pid
+  }
+
+  async getUserAid(pid: string, platform: string){
+    let aid = (await this.ctx.database.get('binding', {'pid': pid,'platform': platform},['aid']))[0].aid
+    return aid
+  }
 }
+declare module 'koishi' {
+  interface Context {
+    idconverter: idconverter
+  }
+}
+
+
+namespace idconverter {
+  export interface Config {}
+
+  export const Config: Schema<Config> = Schema.object({})
+}
+
+export default idconverter
